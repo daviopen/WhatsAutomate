df2d8b26f2a2f9944d821c6a7b73940599d96a72
davi.alves_act <davi.alves_act@prestador.globo>
2023-10-31 15:58:22 -0300

    Criação e estruturação projeto

---
+++ uteis/config.py
@@ -0,0 +1,16 @@
+# config.py
+
+# Arquivo contendo os contatos
+contatos_arquivo = 'contatos.txt'
+
+# Arquivo contendo a mensagem
+mensagem_arquivo = 'mensagem.txt'
+
+# Diretório das imagens
+diretorio_imagens = 'imagens'
+
+# Tempo entre os envios para cada contato
+tempo_entre_envios = 10  # segundos
+
+# Tempo de espera para fechar a guia do navegador
+tempo_fechar_guia = 5  # segundos
